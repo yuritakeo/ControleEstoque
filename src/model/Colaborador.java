8dae0d91aa73a5fadf33ad0f2602d4798f8d4d34
Thiago <Thiago@192.168.25.9>
2018-11-15 20:35:02 -0200

    Autenticar usuario

--- src/model/Colaborador.java
+++ src/model/Colaborador.java
@@ -10,5 +10,43 @@ package model;
  * @author Thiago
  */
 public class Colaborador {
+    private int id;
+    private String nome;
+    private String cpf;
+    private String funcao;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+    
+    
     
 }
---
+++ src/model/Usuario.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Thiago
+ */
+public class Usuario {
+    private int id;
+    private Colaborador colaborador;
+    private String login;
+    private String senha;
+
+    public Usuario(String login , String senha){
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Colaborador getColaborador() {
+        return colaborador;
+    }
+
+    public void setColaborador(Colaborador colaborador) {
+        this.colaborador = colaborador;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+    
+    
+}
